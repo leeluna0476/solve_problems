9c328af08a9320f4e5ba34117508bd2ba63d6fec
seojilee <seojilee@student.42seoul.kr>
2024-12-27 14:33:25 +0900

    two_sum

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -9,9 +9,9 @@ class Solution
         vector<int> twoSum(vector<int>& nums, int target)
         {
             vector<int> answer;
-            unordered_map<int, int>  umap;
 
-            int v_size = nums.size();
+            int                     v_size = nums.size();
+            unordered_map<int, int> umap(v_size);
             for (int i = 0; i < v_size; ++i)
             {
                 unordered_map<int, int>::iterator    it = umap.find(target - nums[i]);
