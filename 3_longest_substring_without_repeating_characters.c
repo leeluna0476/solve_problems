39a4dcad7ca45f73196ac7ba024aaa292cfb9fee
LEE LUNA <seojilee@student.42seoul.kr>
2025-03-27 01:32:30 +0900

    3: add comments

--- 3_longest_substring_without_repeating_characters.c
+++ 3_longest_substring_without_repeating_characters.c
@@ -3,32 +3,38 @@
 
 #define MAX(a, b) a > b ? a : b
 
-static void	init_subidx(short subidx[], const char *str, short s, short e)
+void	init_idxmap(short idxmap[], const char *str, short s, short e)
 {
 	for (short i = s; i <= e; ++i)
-		subidx[str[i]] = -1;
+		idxmap[str[i]] = -1;
 }
 
+/*
+ * 1. s를 순회한다.
+ * 2. s에서 각 문자가 등장하는 인덱스를 idxmap에 저장한다. 새로운 문자를 읽을 때마다 부분열의 길이를 업데이트한다.
+ * 3. 부분열 내에서 문자가 중복으로 등장하면(idxmap[c] != -1) 부분열의 시작 문자부터 해당 문자까지의 모든 문자를 삭제하고 부분열의 길이를 업데이트한다.
+ * 4. 가장 긴 부분열의 길이를 업데이트한다.
+ */
 int lengthOfLongestSubstring(char *s)
 {
-	short	subidx[CHAR_MAX + 1];
-	memset(subidx, -1, sizeof(subidx));
+	short	idxmap[CHAR_MAX + 1];
+	memset(idxmap, -1, sizeof(idxmap));
 
 	int	max_sublen = 0;
 	short	sublen = 0;
 	short	i = 0;
 	while (s[i])
 	{
-		if (subidx[s[i]] == -1)
+		if (idxmap[s[i]] == -1)
 			++sublen;
 		else
 		{
 			short	init_start = i - sublen;
-			sublen = i - subidx[s[i]];
-			init_subidx(subidx, s, init_start, subidx[s[i]]);
+			sublen = i - idxmap[s[i]];
+			init_idxmap(idxmap, s, init_start, idxmap[s[i]]);
 	    }
 		max_sublen = MAX(max_sublen, sublen);
-		subidx[s[i]] = i;
+		idxmap[s[i]] = i;
 		++i;
 	}
 	return max_sublen;
