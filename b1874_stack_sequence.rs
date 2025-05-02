b085f0baa034d9e28fd0d3ff7af00eb3e6a36672
LEE LUNA <seojilee@student.42seoul.kr>
2025-05-03 04:22:32 +0900

    baekjoon 1874: 스택 수열

---
+++ b1874_stack_sequence.rs
@@ -0,0 +1,48 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let n: usize = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .parse()
+        .expect("Invalid number");
+
+    let mut sequence = Vec::<u32>::with_capacity(n);
+    let mut stack = Vec::<u32>::with_capacity(n);
+
+    for _i in 0..n {
+        let num: u32 = lines
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim()
+            .parse()
+            .expect("Invalid number");
+
+        sequence.push(num);
+    }
+
+    let mut printbuf = String::new();
+    let mut the_last = 0;
+    for num in sequence {
+        for i in (the_last + 1)..=num {
+            stack.push(i);
+            printbuf.push_str("+\n");
+        }
+        the_last = u32::max(num, the_last);
+
+        let popped = stack.pop().unwrap();
+        if popped > num {
+            println!("NO");
+            return;
+        }
+        printbuf.push_str("-\n");
+    }
+
+    print!("{printbuf}");
+}
