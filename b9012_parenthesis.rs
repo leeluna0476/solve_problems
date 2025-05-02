e0ddb0ae11a4c22367fa70995e4acb9382964ea7
LEE LUNA <seojilee@student.42seoul.kr>
2025-05-02 19:25:29 +0900

    baekjoon 9012: 괄호

---
+++ b9012_parenthesis.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+    let stdin = io::stdin();
+    stdin
+        .read_line(&mut line)
+        .expect("No line");
+
+    let n: usize = line
+        .trim()
+        .parse()
+        .expect("No number");
+
+    let mut printbuf = String::new();
+    for _i in 0..n {
+        line.clear();
+        stdin
+            .read_line(&mut line)
+            .expect("No line");
+        let trimmed_line = line.trim();
+
+        let mut flag: i8 = 0;
+        for c in trimmed_line.chars() {
+            if c == '(' {
+                flag += 1;
+            } else {
+                flag -= 1;
+            }
+
+            if flag < 0 {
+                break;
+            }
+        }
+
+        if flag == 0 {
+            printbuf.push_str(format!("YES\n").as_str());
+        } else {
+            printbuf.push_str(format!("NO\n").as_str());
+        }
+    }
+
+    print!("{printbuf}");
+}
